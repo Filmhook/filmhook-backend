fbea6d8f8dbdb2dd0798bb1bbbbf1c7f3f22a7e6
Jesu <jesuraj.a@annulartechnologies.com>
2024-05-13 19:07:21 +0530

    Block API changes.

--- src/main/java/com/annular/filmhook/service/impl/BlockServiceImpl.java
+++ src/main/java/com/annular/filmhook/service/impl/BlockServiceImpl.java
@@ -1,13 +1,16 @@
 package com.annular.filmhook.service.impl;
 
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Optional;
+import java.util.Objects;
+
+import com.annular.filmhook.service.MediaFilesService;
+import com.annular.filmhook.util.Utility;
+import com.annular.filmhook.webmodel.FileOutputWebModel;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -46,95 +49,96 @@ public class BlockServiceImpl implements BlockService {
 	FileUtil fileUtil;
 	
 	@Autowired
-	MediaFilesRepository mediaFilesRepository;
+	MediaFilesService mediaFilesService;
 
 	private static final Logger logger = LoggerFactory.getLogger(BlockServiceImpl.class);
 
 	@Override
 	public ResponseEntity<?> addBlock(BlockWebModel blockWebModel) {
-		HashMap<String, Object> response = new HashMap<>();
+		List<BlockWebModel> blockWebModels = new ArrayList<>();
 		try {
 			Block block = new Block();
-			block.setBlockedBy(userDetails.userInfo().getId());
+			block.setBlockedBy(User.builder().userId(blockWebModel.getBlockedBy()).build());
 			block.setStatus(true);
-			block.setBlockedUser(blockWebModel.getBlockedUser());
+			block.setBlockedUser(User.builder().userId(blockWebModel.getBlockedUser()).build());
 			block.setBlockStatus("Blocked");
 			block.setCreatedBy(userDetails.userInfo().getId());
 			blockRepository.save(block);
-			response.put("blockInfo", block);
-			
+
+			blockWebModels = this.transformBlockToBlockWebModel(List.of(block));
 			logger.info("addBlock method end");
-			return ResponseEntity.ok(new Response(1, "Add block successfully", response));
+			return ResponseEntity.ok(new Response(1, "Add block successfully", blockWebModels));
 		} catch (Exception e) {
 			logger.error("Error setting block {}", e.getMessage());
-			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-					.body(new Response(-1, "Error setting block", e.getMessage()));
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new Response(-1, "Error setting block", e.getMessage()));
 		}
 
 	}
 
-	@Override
-	public ResponseEntity<?> getAllBlock() {
+	private List<BlockWebModel> transformBlockToBlockWebModel(List<Block> blockList) {
+		List<BlockWebModel> blockWebModels = new ArrayList<>();
 		try {
-			logger.info("getAllBlockservice start");
+			if(!Utility.isNullOrEmptyList(blockList)) {
+				blockList.stream()
+						.filter(Objects::nonNull)
+						.filter(block -> block.getBlockedUser() != null)
+						.forEach(block -> {
 
-			List<Block> blockData = blockRepository.findByBlockedBy(userDetails.userInfo().getId());
-			List<LinkedHashMap<String, Object>> responseList = new ArrayList<>();
-			for (Block block : blockData) {
-				Integer blockUserId = block.getBlockedUser();
-				if (blockUserId != null) {
-					Optional<User> userOptional = userRepository.findById(blockUserId);
+							BlockWebModel.BlockWebModelBuilder blockWebModel = BlockWebModel.builder()
+									.blockId(block.getBlockId())
+									.blockedBy(block.getBlockedBy().getUserId())
+									.blockedUser(block.getBlockedUser().getUserId())
+									.blockStatus(block.getBlockStatus())
+									.userName(block.getBlockedUser().getName())
+									.gender(block.getBlockedUser().getGender())
+									.createdBy(block.getCreatedBy())
+									.createdOn(block.getCreatedOn())
+									.updatedBy(block.getUpdatedBy())
+									.updatedOn(block.getUpdatedOn());
 
-					if (userOptional.isPresent()) {
-						User user = userOptional.get();
-						Optional<MediaFiles> profilePicOptional = mediaFilesRepository
-								.findByUserIdAndCategory(user.getUserId(), MediaFileCategory.ProfilePic);
+							List<FileOutputWebModel> profilePic = mediaFilesService.getMediaFilesByCategoryAndRefId(MediaFileCategory.ProfilePic, block.getBlockedUser().getUserId());
+							String profilePicUrl = null;
+							if (!Utility.isNullOrEmptyList(profilePic)) {
+								profilePicUrl = s3Util.generateS3FilePath(profilePic.get(0).getFilePath() + profilePic.get(0).getFileType());
+								blockWebModel.profilePicUrl(profilePicUrl);
+							}
 
-						LinkedHashMap<String, Object> pinData = new LinkedHashMap<>();
-						pinData.put("blockUserId", block.getBlockedUser());
-						pinData.put("userId", block.getBlockedBy());;
-						pinData.put("userName", user.getName());
-						pinData.put("userGender", user.getGender());
-						 if (profilePicOptional.isPresent()) {
-		                        MediaFiles mediaFiles = profilePicOptional.get();
-		                        pinData.put("filePathProfile", mediaFiles.getFilePath());
-		                        pinData.put("fileNameProfile", mediaFiles.getFileName());
-		                        pinData.put("fileNameSize", mediaFiles.getFileSize());
-		                        pinData.put("fileNameTypeProfile", mediaFiles.getFileType());
-		                        pinData.put("profilePicUrl", s3Util.getS3BaseURL() + S3Util.S3_PATH_DELIMITER
-		                            + mediaFiles.getFilePath() + mediaFiles.getFileType());
-		                    } else {
-		                        // Handle case where profile picture is not found
-		                        pinData.put("profilePicUrl", null);
-		                    }
-						responseList.add(pinData);
-					} else {
-						logger.warn("User not found for blockUserId: " + blockUserId);
-					}
-				} else {
-					logger.warn("blockUserId is null for userProfilePin: " + block);
-				}
+							blockWebModels.add(blockWebModel.build());
+						});
 			}
-			return ResponseEntity.ok(responseList);
 		} catch (Exception e) {
-			logger.error("getAllBlock service Method Exception {} ", e);
+			e.printStackTrace();
+		}
+		return blockWebModels;
+	}
+
+	@Override
+	public ResponseEntity<?> getAllBlock(Integer userId) {
+		List<BlockWebModel> blockWebModels = new ArrayList<>();
+		try {
+			User userToSearch = User.builder().userId(userId != null ? userId : userDetails.userInfo().getId()).build();
+			List<Block> blockData = blockRepository.findByBlockedBy(userToSearch);
+			return ResponseEntity.ok(this.transformBlockToBlockWebModel(blockData));
+		} catch (Exception e) {
+			logger.error("getAllBlock service Method Exception {} ", e.getMessage());
 			e.printStackTrace();
 			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new Response(-1, "Fail", ""));
 		}
 	}
+
 	@Override
 	public String unBlockProfile(BlockWebModel blockWebModel) {
 		try {
-			Integer currentUser = (userDetails != null && userDetails.userInfo() != null) ? userDetails.userInfo().getId() : null;
-			Block blockedUser = blockRepository.findByBlockedByAndBlockedUser(currentUser, blockWebModel.getBlockedUser());
-			if (blockedUser != null) {
-				blockedUser.setBlockStatus("UnBlocked");
-				blockRepository.saveAndFlush(blockedUser);
+			User currentUser = User.builder().userId(blockWebModel.getBlockedBy()).build();
+			User blockedUser = User.builder().userId(blockWebModel.getBlockedUser()).build();
+			Block blockRowToUpdate = blockRepository.findByBlockedByAndBlockedUser(currentUser, blockedUser);
+			if (blockRowToUpdate != null) {
+				blockRowToUpdate.setBlockStatus("UnBlocked");
+				blockRepository.saveAndFlush(blockRowToUpdate);
 				return "Profile unblocked successfully...";
 			} else {
 				return "Blocked profile not found...";
 			}
-
 		} catch (Exception e) {
 			logger.error("Error at unBlockProfile -> {}", e.getMessage());
 			e.printStackTrace();
