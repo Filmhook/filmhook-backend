d6fff1c914b28eea734e68f2ccc1276f1d0f9655
Benisha BM <benisha.b@annulartechnologies.com>
2025-05-12 12:46:15 +0530

    remove token

--- src/main/java/com/annular/filmhook/security/WebSecurityConfig.java
+++ src/main/java/com/annular/filmhook/security/WebSecurityConfig.java
@@ -77,7 +77,7 @@ public class WebSecurityConfig {
                                         "/user/verify", "/user/resendOtp", "/user/verifyForgotOtp", "/industryUser/addIndustryUserPermanentDetails",
                                         "/api/printName", "/industryUser/getDetails", "/industryUser/addTemporaryDetails","/user/saveCoverPhotos",
                                         "/industryUser/getTemporaryDetails", "/industryUser/addIndustryUserPermanentDetails","/user/saveProfilePhotos",
-                                        "/industryUser/saveIndustryUserFiles", "/industryUser/updateTemporaryDetails","/user/updateUserFlag","/industryUser/saveOneMinuteVideo","/user/updateRerferrralcode")
+                                        "/industryUser/saveIndustryUserFiles", "/industryUser/updateTemporaryDetails","/user/updateUserFlag","/industryUser/saveOneMinuteVideo","/user/updateRerferrralcode","/industryUser/saveGovermentIdProof")
                                 .permitAll()
                                 .anyRequest()
                                 .authenticated()
