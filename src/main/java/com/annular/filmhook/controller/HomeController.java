add86c3cc5569cffd244b561f461dacb4179ba60
Dinesh <mddinesh108@gmail.com>
2025-07-12 15:28:15 +0530

    Homecontroller added for / api

--- src/main/java/com/annular/filmhook/controller/HomeController.java
+++ src/main/java/com/annular/filmhook/controller/HomeController.java
@@ -6,7 +6,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api")
 public class HomeController {
 
     public static final Logger logger = LoggerFactory.getLogger(HomeController.class);
