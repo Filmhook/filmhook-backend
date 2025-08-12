a1fb2ec1da74e7b32c23277d695e7c14f3f8a366
Harshitha-FilmHook <harshithafmpl21@gmail.com>
2025-08-12 13:18:50 +0530

    image

--- src/main/java/com/annular/filmhook/util/MediaConversionUtil.java
+++ src/main/java/com/annular/filmhook/util/MediaConversionUtil.java
@@ -17,8 +17,8 @@ public class MediaConversionUtil {
 	public static void convertToWebP(String inputPath, String outputPath) throws IOException, InterruptedException {
 	    ProcessBuilder processBuilder = new ProcessBuilder(
 
- // "/usr/bin/cwebp",
-"C:\\Program Files\\webpUtil\\libwebp-1.5.0-windows-x64\\bin\\cwebp.exe",
+"/usr/bin/cwebp",
+//"C:\\Program Files\\webpUtil\\libwebp-1.5.0-windows-x64\\bin\\cwebp.exe",
 
 	        "-q", "90",
 	        inputPath,
