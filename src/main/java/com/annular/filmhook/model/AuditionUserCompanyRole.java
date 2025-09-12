3e50978ef8d710fd98450047a06e5b3064b7f841
Harshitha-FilmHook <harshithafmpl21@gmail.com>
2025-09-12 15:10:06 +0530

    Audition Access code and login

--- src/main/java/com/annular/filmhook/model/AuditionUserCompanyRole.java
+++ src/main/java/com/annular/filmhook/model/AuditionUserCompanyRole.java
@@ -8,7 +8,7 @@ import javax.persistence.*;
 import lombok.*;
 
 @Entity
-@Table(name = "user_company_roles")
+@Table(name = "audition_company_roles")
 @Getter
 @Setter
 @NoArgsConstructor
@@ -16,19 +16,36 @@ import lombok.*;
 @Builder
 public class AuditionUserCompanyRole {
 
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Integer id;
 
-    private Long userId;   
-    private Long companyId;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "owner_id", nullable = false) // The company owner
+    private User owner;
 
-    private String designation; 
-    
-    private String status;              
-    private String createdBy;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "company_id", nullable = false)
+    private AuditionCompanyDetails company;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "assigned_user_id", nullable = false) // The user who will get access
+    private User assignedUser;
+
+    private String designation;
+
+    @Column(unique = true, nullable = false)
+    private String accessKey;
+
+    private Boolean status; // ACTIVE, INACTIVE, REVOKED
+
+    private Integer createdBy;
     private LocalDateTime createdDate;
-    private String updatedBy;
+    private Integer updatedBy;
     private LocalDateTime updatedDate;
+    
+    private String filmHookCode;
+
 }
 
--- src/main/java/com/annular/filmhook/service/AuditionCompanyService.java
+++ src/main/java/com/annular/filmhook/service/AuditionCompanyService.java
@@ -5,17 +5,20 @@ package com.annular.filmhook.service;
 import java.util.List;
 
 import com.annular.filmhook.model.AuditionCompanyDetails;
+import com.annular.filmhook.model.User;
 import com.annular.filmhook.webmodel.AuditionCompanyDetailsDTO;
+import com.annular.filmhook.webmodel.AuditionUserCompanyRoleDTO;
 
 
 public interface AuditionCompanyService {
 	 AuditionCompanyDetailsDTO saveCompany(AuditionCompanyDetailsDTO dto);
 	 List<AuditionCompanyDetailsDTO> getCompaniesByUserId(Integer userId);
 	 List<AuditionCompanyDetailsDTO> getAllCompanies();
-	 List<AuditionCompanyDetailsDTO> getAllActivePendingCompanies();
+	 List<AuditionCompanyDetailsDTO> getAllPendingCompanies();
 	 AuditionCompanyDetails updateVerificationStatus(Integer companyId, boolean approved);
 	 AuditionCompanyDetailsDTO markCompanyAsContinued(Integer companyId, Integer userId);
-	 
+	 AuditionUserCompanyRoleDTO assignAccess(AuditionUserCompanyRoleDTO request);
+	 Object handleAuditionAccess(User loggedUser, String filmHookCode, String designation, String accessCode);
 //	    AuditionCompanyDetailsDTO updateCompany(Long id, AuditionCompanyDetailsDTO dto);
 //
 //	    AuditionCompanyDetailsDTO getCompanyById(Long id);
