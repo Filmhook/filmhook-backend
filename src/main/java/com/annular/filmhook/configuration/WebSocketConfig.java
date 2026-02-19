709e6efdc85b26c69831875a51a00d3c32812b97
Harshitha-FilmHook <harshithafmpl21@gmail.com>
2026-02-19 10:57:32 +0530

    websocke

--- src/main/java/com/annular/filmhook/configuration/WebSocketConfig.java
+++ src/main/java/com/annular/filmhook/configuration/WebSocketConfig.java
@@ -31,7 +31,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOrigins("https://www.filmhookapps.com");
+                .setAllowedOrigins("*");
     }
 
     // ✅ Message Broker Config
