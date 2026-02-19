31e7fa0fd1b72ef772022a58267417c97ed9924e
Harshitha-FilmHook <harshithafmpl21@gmail.com>
2026-02-19 14:44:56 +0530

    websocket

--- src/main/java/com/annular/filmhook/configuration/WebSocketAuthInterceptor.java
+++ src/main/java/com/annular/filmhook/configuration/WebSocketAuthInterceptor.java
@@ -5,6 +5,7 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.http.server.ServletServerHttpRequest;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.server.HandshakeInterceptor;
@@ -24,10 +25,10 @@ public class WebSocketAuthInterceptor implements HandshakeInterceptor {
                                    WebSocketHandler wsHandler,
                                    Map<String, Object> attributes) {
 
-    	if (request instanceof org.springframework.http.server.ServletServerHttpRequest) {
+    	if (request instanceof ServletServerHttpRequest) {
 
-    	    org.springframework.http.server.ServletServerHttpRequest servletRequest =
-    	            (org.springframework.http.server.ServletServerHttpRequest) request;
+    	   ServletServerHttpRequest servletRequest =
+    	            (ServletServerHttpRequest) request;
 
     	    String sessionToken =
     	            servletRequest.getServletRequest()
