de44230a6ece68e73ea947be54f9d1212579f247
Dinesh-FilmHook <dineshfilmhook@gmail.com>
2026-03-04 18:40:15 +0530

    Added call history

--- src/main/java/com/annular/filmhook/repository/CallLogRepository.java
+++ src/main/java/com/annular/filmhook/repository/CallLogRepository.java
@@ -1,5 +1,7 @@
 package com.annular.filmhook.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -12,6 +14,9 @@ public interface CallLogRepository extends JpaRepository<CallLog, Integer> {
 
     @Query("SELECT COUNT(c) > 0 FROM CallLog c WHERE c.receiverId = :uid AND c.status = 'initiated'")
     boolean isUserInActiveCall(@Param("uid") Integer uid);
+    
+    @Query("SELECT c FROM CallLog c WHERE c.callerId = :userId OR c.receiverId = :userId ORDER BY c.startTime DESC")
+    List<CallLog> findCallHistory(@Param("userId") Integer userId);
 
     CallLog findByChannelName(String channelName);
 }
--- src/main/java/com/annular/filmhook/repository/GroupCallMemberRepository.java
+++ src/main/java/com/annular/filmhook/repository/GroupCallMemberRepository.java
@@ -3,11 +3,28 @@ package com.annular.filmhook.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.annular.filmhook.model.GroupCallMember;
 
 public interface GroupCallMemberRepository extends JpaRepository<GroupCallMember, Integer> {
-	GroupCallMember findByGroupCallIdAndUserId(Integer groupCallId, Integer userId);
+	//GroupCallMember findByGroupCallIdAndUserId(Integer groupCallId, Integer userId);
 
 	List<GroupCallMember> findByGroupCallId(Integer groupCallId);
+
+	@Query("SELECT u.id, u.name FROM GroupCallMember g JOIN User u ON u.id = g.userId " +
+			"WHERE g.groupCallId = :groupCallId AND g.userId <> :userId")
+	List<Object[]> findGroupMembers(@Param("groupCallId") Integer groupCallId,
+			@Param("userId") Integer userId);
+	
+	@Query(value =
+	        "SELECT * FROM group_call_members " +
+	        "WHERE group_call_id = :groupCallId AND user_id = :userId " +
+	        "LIMIT 1",
+	        nativeQuery = true)
+	GroupCallMember findByGroupCallIdAndUserId(@Param("groupCallId") Integer groupCallId,
+	                                           @Param("userId") Integer userId);
+
+
 }
